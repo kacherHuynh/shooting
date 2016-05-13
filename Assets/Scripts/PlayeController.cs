0166259c5b79abe1fb01da2f7f6e81ea2549385e
Tai Huynh <taihh@fireflyinnov.com>
2016-05-13 10:24:51 +0700

    Add puppet2D

--- Assets/Scripts/PlayeController.cs
+++ Assets/Scripts/PlayeController.cs
@@ -6,7 +6,7 @@ public class PlayeController : MonoBehaviour {
 
 	public float speed = 10.0f;
 	public float bulletSpeed = 20.0f;
-	public Rigidbody2D rocket;
+	public Object rocket;
 	public Transform shootingPos;
 
 	public AudioClip shootingSound;
@@ -18,36 +18,27 @@ public class PlayeController : MonoBehaviour {
 
 	void Update (){ 
 		
-//		if (Input.GetButtonDown("Fire1")) {
-//			Rigidbody2D bulletInstance = Instantiate (rocket, transform.position, Quaternion.Euler(Vector3.zero)) as Rigidbody2D;
-//			bulletInstance.velocity = new Vector2 (bulletSpeed, 0);
-//		}
+		if ((CrossPlatformInputManager.GetButtonDown ("Shoot")) || Input.GetKeyDown(KeyCode.Space)) {
+			GameObject bulletInstance = GameObject.Instantiate(rocket) as GameObject;
+			bulletInstance.GetComponent<Rigidbody2D>().velocity = new Vector2 (bulletSpeed, 0);
+			GameObject.FindGameObjectWithTag ("GameManager").GetComponent<AudioSource> ().PlayOneShot (shootingSound,1);
+		}
 	}
 
 	void FixedUpdate () {
 		
 		float moveVertical = CrossPlatformInputManager.GetAxis ("Vertical") * speed ;
 
-		Debug.Log (moveVertical);
-
 		if (moveVertical > 0) {
-			transform.position = Vector3.up * speed * Time.deltaTime;
+//			transform.position = Vector3.up * speed * Time.deltaTime;
 		} else {
 //			destination = Vector3.down * moveVertical;
 //			transform.position = Vector3.Lerp (transform.position, destination, Time.deltaTime);
 		}
 
-//		Vector3 movement = new Vector3 (0.0f, moveVertical, 0.0f);
-//		rigid.velocity = movement;
+		Vector3 movement = new Vector3 (0.0f, moveVertical, 0.0f);
+		rigid.velocity = movement;
 
-
-
-
-		if (CrossPlatformInputManager.GetButtonDown ("Shoot")) {
-			Rigidbody2D bulletInstance = Instantiate (rocket, shootingPos.position, Quaternion.Euler(Vector3.zero)) as Rigidbody2D;
-			bulletInstance.velocity = new Vector2 (bulletSpeed, 0);
-			GameObject.FindGameObjectWithTag ("GameManager").GetComponent<AudioSource> ().PlayOneShot (shootingSound,1);
-		}
 	}
 
 
